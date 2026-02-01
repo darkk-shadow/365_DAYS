5176823d5239474610a7516ef8f4268e60950981
Balaji <balajimahesh0818@gmail.com>
2026-02-01 20:45:06 +0530

    JAVA_365

---
+++ src/java_365/Largest_Number.java
@@ -0,0 +1,15 @@
+package java_365;
+
+public class Largest_Number {
+	public static void main(String[] args) {
+		int arr[]= {0,1,211,3,4,5,99,1022};
+		
+		int max=arr[0];
+		for(int i=0;i<=arr.length-1;i++) { //for(int i=0;i<arr.length;i++)This logic also fine
+			if(arr[i]>max)
+			max=arr[i];
+		}
+		System.out.println(max);
+	}
+
+}
---
+++ src/java_365/Second_largest2.java
@@ -0,0 +1,7 @@
+package java_365;
+
+public class Second_largest2 {
+	public static void main(String[] args) {
+	}
+
+}
