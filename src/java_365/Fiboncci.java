e4c0c4e6bcc51fac99b18db8917ba037a3ce42d4
Balaji <balajimahesh0818@gmail.com>
2026-02-18 22:29:22 +0530

    day_14

--- src/java_365/Fiboncci.java
+++ src/java_365/Fiboncci.java
@@ -1,24 +1,24 @@
 package java_365;
+
 import java.util.*;
+
 public class Fiboncci {
-	static Scanner sc=new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		System.out.print("Enter the range : ");
-		int range=sc.nextInt();
-		int a=0;
-		int b=1;
+		int range = sc.nextInt();
+		int a = 0;
+		int b = 1;
 		System.out.print("Fibonacci series : ");
-	
-		for(int i=0;i<=range;i++) {
-			System.out.print(a+" ");
 
-			int c=a+b;
-			a=b;
-			b=c;
-			
-			
-			
-			
+		for (int i = 0; i <= range; i++) {
+			System.out.print(a + " ");
+
+			int c = a + b;
+			a = b;
+			b = c;
+
 		}
 	}
 
