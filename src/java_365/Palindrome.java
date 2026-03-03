520b9e1ad6d33b3ba2d161fc552a26046112c4e4
Balaji <153199140+darkk-shadow@users.noreply.github.com>
2026-03-03 22:19:24 +0530

    Refactor Palindrome class and improve output messages

--- src/java_365/Palindrome.java
+++ src/java_365/Palindrome.java
@@ -1,23 +1,27 @@
-package java_365;
+package practice;
+
 import java.util.*;
+
 public class Palindrome {
-	static Scanner sc=new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.print("Enter the number : ");
-		int num=sc.nextInt();
-		int temp=num;
-		int rev = 0;
-		for(;num!=0;num=num/10) {
-			int digit=num%10;
-			rev=rev*10+digit;
-		}
-		if(temp==rev) {
-			System.out.print(temp+" is a Palindrome");
-		}
-		else {
-			System.out.print(temp+" is not an Palindrome");
-		}
+		System.out.print("enter the num : ");
+		int num = sc.nextInt();
+		boolean res = isPalindrome(num);
+		if (res == true)
+			System.out.println(num + " it is an palindrome");
+		else
+			System.out.println(num + " it is not an palindrome");
 	}
 
+	public static boolean isPalindrome(int num) {
+		int temp = num;
+		int rev = 0;
+		while (num > 0) {
+			rev = rev * 10 + num % 10;
+			num = num / 10;
+		}
+		return temp == rev;
+	}
 }
